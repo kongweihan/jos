6bc5e778c33c0c8f13b9efe4b9020db6ad8a1132
Weihan Kong <kongweihan@gmail.com>
2018-03-20 21:58:38 -0400

    Lab 4A: system calls for env creation
    Wrap page allocation and virtual memory management functions as several
    syscalls, which allows a user program to create a new env.
    
    These functions checks the syscall arguments and call underlying
    memory management kernel functions.
    
    New env copies the trapframe of the parent env except the return value
    is 0, so that the new child env's fork() returns 0.

--- kern/syscall.c
+++ kern/syscall.c
@@ -71,6 +71,27 @@ sys_yield(void)
 	sched_yield();
 }
 
+static void copy_env_tf(struct Env* parent, struct Env* child) {
+	child->env_tf.tf_regs.reg_edi = parent->env_tf.tf_regs.reg_edi;
+	child->env_tf.tf_regs.reg_esi = parent->env_tf.tf_regs.reg_esi;
+	child->env_tf.tf_regs.reg_ebp = parent->env_tf.tf_regs.reg_ebp;
+	child->env_tf.tf_regs.reg_oesp = parent->env_tf.tf_regs.reg_oesp;
+	child->env_tf.tf_regs.reg_ebx = parent->env_tf.tf_regs.reg_ebx;
+	child->env_tf.tf_regs.reg_edx = parent->env_tf.tf_regs.reg_edx;
+	child->env_tf.tf_regs.reg_ecx = parent->env_tf.tf_regs.reg_ecx;
+	child->env_tf.tf_regs.reg_eax = parent->env_tf.tf_regs.reg_eax;
+
+	child->env_tf.tf_es = parent->env_tf.tf_es;
+	child->env_tf.tf_ds = parent->env_tf.tf_ds;
+	child->env_tf.tf_trapno = parent->env_tf.tf_trapno;
+	child->env_tf.tf_err = parent->env_tf.tf_err;
+	child->env_tf.tf_eip = parent->env_tf.tf_eip;
+	child->env_tf.tf_cs = parent->env_tf.tf_cs;
+	child->env_tf.tf_eflags = parent->env_tf.tf_eflags;
+	child->env_tf.tf_esp = parent->env_tf.tf_esp;
+	child->env_tf.tf_ss = parent->env_tf.tf_ss;
+}
+
 // Allocate a new environment.
 // Returns envid of new environment, or < 0 on error.  Errors are:
 //	-E_NO_FREE_ENV if no free environment is available.
@@ -85,7 +106,16 @@ sys_exofork(void)
 	// will appear to return 0.
 
 	// LAB 4: Your code here.
-	panic("sys_exofork not implemented");
+	struct Env *env;
+	int r = env_alloc(&env, curenv->env_id);
+	if (r == 0) {
+		env->env_status = ENV_NOT_RUNNABLE;
+		copy_env_tf(curenv, env);
+		env->env_tf.tf_regs.reg_eax = 0;
+		return env->env_id;
+	} else {
+		return r;
+	}
 }
 
 // Set envid's env_status to status, which must be ENV_RUNNABLE
@@ -105,7 +135,20 @@ sys_env_set_status(envid_t envid, int status)
 	// envid's status.
 
 	// LAB 4: Your code here.
-	panic("sys_env_set_status not implemented");
+	if (status != ENV_RUNNABLE && status != ENV_NOT_RUNNABLE) {
+		return -E_INVAL;
+	}
+	if (envid < 0) {
+		return -E_INVAL;
+	}
+
+	struct Env *env;
+	int r = envid2env(envid, &env, 1);
+	env->env_status = status;
+	if (r < 0) {
+		return r;
+	}
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -150,7 +193,34 @@ sys_page_alloc(envid_t envid, void *va, int perm)
 	//   allocated!
 
 	// LAB 4: Your code here.
-	panic("sys_page_alloc not implemented");
+	struct Env* env;
+	int r = envid2env(envid, &env, 1);
+	if (r < 0) {
+		return r;
+	}
+	if ((uintptr_t) va >= UTOP || (uintptr_t) va % PGSIZE != 0) {
+		return -E_INVAL;
+	}
+	if ((perm & PTE_U) == 0 && (perm & PTE_P) == 0) {
+		return -E_INVAL;
+	}
+	if (perm & ~PTE_U & ~PTE_P & ~PTE_AVAIL & ~PTE_W) {
+		return -E_INVAL;
+	}
+
+	struct PageInfo *pp = page_alloc(ALLOC_ZERO);
+	if (pp == NULL) {
+		return -E_NO_MEM;
+	}
+
+	// Note, no need to increment pp_ref, because page_insert should and will do
+	// it. If page_insert fails, just free the page, no need to decrement pp_ref
+	r = page_insert(env->env_pgdir, pp, va, perm);
+	if (r < 0) {
+		page_free(pp);
+		return r;
+	}
+	return 0;
 }
 
 // Map the page of memory at 'srcva' in srcenvid's address space
@@ -181,7 +251,43 @@ sys_page_map(envid_t srcenvid, void *srcva,
 	//   check the current permissions on the page.
 
 	// LAB 4: Your code here.
-	panic("sys_page_map not implemented");
+	struct Env *dstenv, *srcenv;
+	int r = envid2env(srcenvid, &srcenv, 1);
+	if (r < 0) {
+		return r;
+	}
+	r = envid2env(dstenvid, &dstenv, 1);
+	if (r < 0) {
+		return r;
+	}
+
+	if ((uintptr_t) srcva >= UTOP || (uintptr_t) srcva % PGSIZE != 0 ||
+		(uintptr_t) dstva >= UTOP || (uintptr_t) dstva % PGSIZE != 0) {
+		return -E_INVAL;
+	}
+	if ((perm & PTE_U) == 0 && (perm & PTE_P) == 0) {
+		return -E_INVAL;
+	}
+	if (perm & ~PTE_U & ~PTE_P & ~PTE_AVAIL & ~PTE_W) {
+		return -E_INVAL;
+	}
+
+	pte_t *pte;
+	struct PageInfo* pp = page_lookup(srcenv->env_pgdir, srcva, &pte);
+	if (pp == NULL) {
+		return -E_INVAL;
+	}
+
+	// Return error if dst page is read-only but perm is not
+	if ((*pte & PTE_W) == 0 && (perm & PTE_W)) {
+		return -E_INVAL;
+	}
+
+	r = page_insert(dstenv->env_pgdir, pp, dstva, perm);
+	if (r < 0) {
+		return r;
+	}
+	return 0;
 }
 
 // Unmap the page of memory at 'va' in the address space of 'envid'.
@@ -197,7 +303,17 @@ sys_page_unmap(envid_t envid, void *va)
 	// Hint: This function is a wrapper around page_remove().
 
 	// LAB 4: Your code here.
-	panic("sys_page_unmap not implemented");
+	struct Env *env;
+	int r = envid2env(envid, &env, 1);
+	if (r < 0) {
+		return r;
+	}
+	if ((uintptr_t) va >= UTOP || (uintptr_t) va % PGSIZE != 0) {
+		return -E_INVAL;
+	}
+
+	page_remove(env->env_pgdir, va);
+	return 0;
 }
 
 // Try to send 'value' to the target env 'envid'.
@@ -284,6 +400,16 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		case SYS_yield:
 			sys_yield();
 			return 0;
+		case SYS_exofork:
+			return sys_exofork();
+		case SYS_env_set_status:
+			return sys_env_set_status((envid_t) a1, a2);
+		case SYS_page_alloc:
+			return sys_page_alloc((envid_t) a1, (void*) a2, a3);
+		case SYS_page_map:
+			return sys_page_map((envid_t) a1, (void*) a2, (envid_t) a3, (void*) a4, a5);
+		case SYS_page_unmap:
+			return sys_page_unmap((envid_t) a1, (void*) a2);
 		default:
 			return -E_INVAL;
 	}
